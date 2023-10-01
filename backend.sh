fda3d4faece96339f57c0dd05fb60c44c8948f82
Sriharsha6197 <m.sriharsha97@gmail.com>
2023-10-01 16:48:33 +0530

    backend

--- backend.sh
+++ backend.sh
@@ -2,8 +2,8 @@ echo downloading nodejs repo
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash       >> /tmp/expense-backend.log
 
 echo installing nodejs
-dnf install nodejs -y
-cp backend.service /etc/systemd/system/backend.service
+dnf install nodejs -y                                        >> /tmp/expense-backend.log
+cp backend.service /etc/systemd/system/backend.service       
 
 
 echo adding user
@@ -12,24 +12,24 @@ rm-rf /app
 mkdir /app
 
 echo downloading the backend code from s3
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip      >> /tmp/expense-backend.log
 
 echo unzipping backend downloaded file
 cd /app
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip                                        >> /tmp/expense-backend.log
 
 echo installing dependencies
-npm install
+npm install                                                   >> /tmp/expense-backend.log
 
 
 echo loading user and enabling and starting backend service
 systemctl daemon-reload
 systemctl enable backend 
-systemctl start backend
+systemctl start backend                                       >> /tmp/expense-backend.log
 
 echo loading to the database
-dnf install mysql -y
-mysql -h mysql.sriharsha.shop -uroot -pSriharsha@1 < /app/schema/backend.sql 
+dnf install mysql -y                                         >> /tmp/expense-backend.log
+mysql -h mysql.sriharsha.shop -uroot -pSriharsha@1 < /app/schema/backend.sql   >> /tmp/expense-backend.log
 
 
 
