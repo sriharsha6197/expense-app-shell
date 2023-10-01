373f2a1f6c1ab74291dfb5f6a0e4c4ecc2363716
Sriharsha6197 <m.sriharsha97@gmail.com>
2023-10-01 16:15:53 +0530

    loading output to a file

--- frontend.sh
+++ frontend.sh
@@ -1,21 +1,21 @@
 echo Installing nginx
-dnf install nginx -y 
+dnf install nginx -y                  > /tmp/expense.log
 
 echo enabling and restarting nginx
-systemctl enable nginx 
-systemctl start nginx 
+systemctl enable nginx                > /tmp/expense.log
+systemctl start nginx                 > /tmp/expense.log
 
 rm -rf /usr/share/nginx/html/* 
 
-echo download the code content from s3
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+echo download the code content from s3    
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip   >/tmp/expense.log
 
 echo Setting up configuration file
 cp expense.conf /etc/nginx/default.d/expense.conf
 
-echo unzipping the downloaded code
+echo unzipping the downloaded code     
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip                  
 
 echo restarting nginx
-systemctl restart nginx 
+systemctl restart nginx                    > /tmp/expense.log
