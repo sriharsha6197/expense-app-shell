29d028ff367ee70a49e344c3f73c6778f9d718cc
Sriharsha6197 <m.sriharsha97@gmail.com>
2023-10-01 16:58:01 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,18 +1,18 @@
 echo disabling module of mysql which is already there
-dnf module disable mysql -y
+dnf module disable mysql -y                  >> /tmp/expense.log
 
 echo setting up mysql repo
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp mysql.repo /etc/yum.repos.d/mysql.repo    
 
 
 echo installing mysql
-dnf install mysql-community-server -y
+dnf install mysql-community-server -y         >> /tmp/expense.log
 
 
 echo enabling and starting mysql
-systemctl enable mysqld
-systemctl start mysqld  
+systemctl enable mysqld                     >> /tmp/expense.log
+systemctl start mysqld                      >> /tmp/expense.log
 
 echo setting up password
-mysql_secure_installation --set-root-pass Sriharsha@1
+mysql_secure_installation --set-root-pass Sriharsha@1  >> /tmp/expense.log
 
