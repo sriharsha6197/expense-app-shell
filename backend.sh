0f93b52a5c4a8cb0b2a616cf653ee0ee6e3750fe
Sriharsha6197 <m.sriharsha97@gmail.com>
2023-10-02 17:28:42 +0530

    sh

--- backend.sh
+++ backend.sh
@@ -3,33 +3,41 @@ component=backend
 
 echo downloading nodejs repo
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash       >> $log_file
+echo $?
 
 echo installing nodejs
 dnf install nodejs -y                                        >> $log_file
+echo $?
 cp backend.service /etc/systemd/system/backend.service       
 
 
 echo adding user
 useradd expense                                              >> $log_file
+echo $?
 rm -rf /app
 
 mkdir /app
 cd /app
 
 download_and_extract
+echo $?
 
 echo installing dependencies
 npm install                                                   >> $log_file
+echo $?
 
 
 echo loading user and enabling and starting $component service
 systemctl daemon-reload                                       >> $log_file
 systemctl enable backend                                      >> $log_file
 systemctl start backend                                       >> $log_file
+echo $?
 
 echo loading to the database
 dnf install mysql -y                                         >> $log_file
+echo $?
 mysql -h mysql.sriharsha.shop -uroot -pSriharsha@1 < /app/schema/backend.sql   >> $log_file
+echo $?
 
 
 
--- frontend.sh
+++ frontend.sh
@@ -3,18 +3,23 @@ component=frontend
 
 echo Installing nginx
 dnf install nginx -y                  >> $log_file
+echo $?
 
 echo enabling and restarting nginx
 systemctl enable nginx                >> $log_file
 systemctl start nginx                 >> $log_file
+echo $?
 
 echo Setting up configuration file
 cp expense.conf /etc/nginx/default.d/expense.conf
+echo $?
 
 rm -rf /usr/share/nginx/html/* 
 cd /usr/share/nginx/html
 
 download_and_extract
+echo $?
 
 echo restarting nginx
 systemctl restart nginx                    >> $log_file
+echo $?
