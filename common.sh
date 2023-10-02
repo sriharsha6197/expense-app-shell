2f729605f2e2bb5ae946083c590a38faa78e9d11
Sriharsha6197 <m.sriharsha97@gmail.com>
2023-10-02 16:42:14 +0530

    ch

--- common.sh
+++ common.sh
@@ -1,11 +1,11 @@
-log_file = /tmp/expense.log
+log_file=/tmp/expense.log
 
 download and extract(){
 
-echo download the code content from s3    
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip   >> $log_file
+echo download the $component code from s3    
+curl -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip   >> $log_file
 
 echo unzipping the downloaded code     
-unzip /tmp/frontend.zip                 >> $log_file
+unzip /tmp/$component.zip                 >> $log_file
 
 }
