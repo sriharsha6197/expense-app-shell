714d5a1a86d717d37d0bbb121511ef9527631b8e
Sriharsha6197 <m.sriharsha97@gmail.com>
2023-10-02 16:13:53 +0530

    ch

--- backend.sh
+++ backend.sh
@@ -1,8 +1,8 @@
 echo downloading nodejs repo
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash       >> log_file
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash       >> $log_file
 
 echo installing nodejs
-dnf install nodejs -y                                        >> log_file
+dnf install nodejs -y                                        >> $log_file
 cp backend.service /etc/systemd/system/backend.service       
 
 
@@ -12,24 +12,24 @@ rm-rf /app
 mkdir /app
 
 echo downloading the backend code from s3
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip      >> log_file
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip      >> $log_file
 
 echo unzipping backend downloaded file
 cd /app
-unzip /tmp/backend.zip                                        >> log_file
+unzip /tmp/backend.zip                                        >> $log_file
 
 echo installing dependencies
-npm install                                                   >> log_file
+npm install                                                   >> $log_file
 
 
 echo loading user and enabling and starting backend service
-systemctl daemon-reload                                       >> log_file
-systemctl enable backend                                      >> log_file
-systemctl start backend                                       >> log_file
+systemctl daemon-reload                                       >> $log_file
+systemctl enable backend                                      >> $log_file
+systemctl start backend                                       >> $log_file
 
 echo loading to the database
-dnf install mysql -y                                         >> log_file
-mysql -h mysql.sriharsha.shop -uroot -pSriharsha@1 < /app/schema/backend.sql   >> log_file
+dnf install mysql -y                                         >> $log_file
+mysql -h mysql.sriharsha.shop -uroot -pSriharsha@1 < /app/schema/backend.sql   >> $log_file
 
 
 
--- mysql.sh
+++ mysql.sh
@@ -1,18 +1,18 @@
 echo disabling module of mysql which is already there
-dnf module disable mysql -y                  >> log_file
+dnf module disable mysql -y                  >> $log_file
 
 echo setting up mysql repo
 cp mysql.repo /etc/yum.repos.d/mysql.repo    
 
 
 echo installing mysql
-dnf install mysql-community-server -y         >> log_file
+dnf install mysql-community-server -y         >> $log_file
 
 
 echo enabling and starting mysql
-systemctl enable mysqld                     >> log_file
-systemctl start mysqld                      >> log_file
+systemctl enable mysqld                     >> $log_file
+systemctl start mysqld                      >> $log_file
 
 echo setting up password
-mysql_secure_installation --set-root-pass Sriharsha@1  >> log_file
+mysql_secure_installation --set-root-pass Sriharsha@1  >> $log_file
 
