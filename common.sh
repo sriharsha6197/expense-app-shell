5445e971dcd708471f208f45063d45585c94c909
Sriharsha6197 <m.sriharsha97@gmail.com>
2023-10-04 20:41:48 +0530

    changes

--- common.sh
+++ common.sh
@@ -3,19 +3,11 @@ log_file=/tmp/expense.log
 download_and_extract() {
 echo download the $component code from s3    
 curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip   >> $log_file
-if [ $? -eq 0 ]; then
-   echo -e "\e[32mSUCCESS\e[0m"
-else
-   echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check
 
 echo unzipping the downloaded code     
 unzip /tmp/$component.zip                 >> $log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check
 }
 
 status_check() {
--- frontend.sh
+++ frontend.sh
@@ -3,43 +3,23 @@ component=frontend
 
 echo Installing nginx
 dnf install nginx -y                  >> $log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check
 
 echo enabling and restarting nginx
 systemctl enable nginx                >> $log_file
 systemctl start nginx                 >> $log_file
-if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-else
-    echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check
 
 echo Setting up configuration file
 cp expense.conf /etc/nginx/default.d/expense.conf
-if [ $? -eq 0 ]; then
-   echo -e "\e[32mSUCCESS\e[0m"
-else
-   echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check
 
 rm -rf /usr/share/nginx/html/* 
 cd /usr/share/nginx/html
 
 download_and_extract
-if [ $? -eq 0 ]; then
-   echo -e "\e[32mSUCCESS\e[0m"
-else
-   echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check
 
 echo restarting nginx
 systemctl restart nginx                    >> $log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check
