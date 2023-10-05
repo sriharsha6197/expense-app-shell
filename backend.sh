cc1630a9ca86ad183ee4c919b4717f4f28a495d6
Sriharsha6197 <m.sriharsha97@gmail.com>
2023-10-06 00:03:30 +0530

    changes

--- backend.sh
+++ backend.sh
@@ -1,52 +1,52 @@
+set -x
 source common.sh
 component=backend
 
-type npm &>>$log_file
+type npm                                                       &>>$log_file
 if [ $? -ne 0 ]; then
-  echo Install NodeJS Repos
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
-  stat_check
+echo downloading nodejs repo
 
-  echo Install NodeJS
-  dnf install nodejs -y &>>$log_file
-  stat_check
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash       &>>$log_file
+  status_check
+
+  echo installing nodejs
+  dnf install nodejs -y                                        &>>$log_file
+  status_check
 fi
+cp backend.service /etc/systemd/system/backend.service       
 
-echo Copy Backend Service File
-cp backend.service /etc/systemd/system/backend.service &>>$log_file
-stat_check
 
-echo Add Application User
-id expense &>>$log_file
+echo adding user
+id expense                                                    &>>$log_file
 if [ $? -ne 0 ]; then
-  useradd expense &>>$log_file
-fi
-stat_check
+useradd expense
+fi                                                            &>>$log_file
+status_check
 
-echo Clean App Content
-rm -rf /app &>>$log_file
-stat_check
+rm -rf /app
 
 mkdir /app
 cd /app
 
 download_and_extract
 
-echo Download Dependencies
-npm install &>>$log_file
-stat_check
 
-echo Start Backend Service
-systemctl daemon-reload &>>$log_file
-systemctl enable backend &>>$log_file
-systemctl restart backend &>>$log_file
-stat_check
+echo installing dependencies
+npm install                                                   &>>$log_file
 
-echo Install MySQL Client
-dnf install mysql -y &>>$log_file
-stat_check
 
-echo Load Schema
+
+echo loading user and enabling and starting $component service
+systemctl daemon-reload                                       &>> $log_file
+systemctl enable backend                                      &>> $log_file
+systemctl start backend                                       &>> $log_file
+status_check
+
+echo Install mysql client
+dnf install mysql -y                                         &>> $log_file
+status_check
+
+echo load schema
 mysql_root_password=$1
-mysql -h mysql.sriharsha.shop -uroot -p$mysql_root_password < /app/schema/backend.sql &>>$log_file
-stat_check
+mysql -h mysql.sriharsha.shop -uroot -p$mysql_root_password < /app/schema/backend.sql     &>>$log_file
+status_check
